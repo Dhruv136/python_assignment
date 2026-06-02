b11bf1dcf526b14865c501dc5c6ca914238c78f1
Dhruv136 <kokchachinu136@gmail.com>
2026-06-02 22:55:36 +0530

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,101 @@
+#<<<<<<<<<<< Question 1>>>>>>>>>>>>>>>>
+
+# integer(int) is used to store integer values like 10,30,203
+# float(float) is used to store floating values , point valuesm
+# string(string) string is used to store any word , character
+# boolean (bool) is used to store bool values like true and false
+
+# <<<<<<<<<<<<<<<<<<Question 2>>>>>>>>>>>>>>>>>
+
+name="dhruv"
+Age="20"
+city="Ramgarh"
+print(name )
+print(Age )
+print(city )
+
+# <<<<<<<<<<<<<<<<Question 3>>>>>>>>>>>>>>>>>>
+
+name = input("enter ur name broi ")
+name.upper()
+print(name)
+print("total number of character in name is ",len(name))
+
+# <<<<<<<<<<<<<<<<  Question 4 >>>>>>>>>>>>>>>>>>>
+
+# 1 upper()  Converts all characters to uppercase
+name = "dhruv"
+print(name.upper())
+
+
+# 2 lower()  Converts all characters to lowercase
+text = "HELLO WORLD"
+print(text.lower()) 
+
+
+# 3 replace() Replaces one part of a string with another
+sentence= "I like Java"
+print(sentence.replace("Java", "Python"))
+
+
+# 4 startswith() Checks if a string starts with given text
+word = "Python"
+print(word.startswith("Py"))  
+
+
+# 5 endswith()  Checks if a string ends with given text
+file = "report"
+print(file.endswith("rt"))  
+ 
+# <<<<<<<<<<<<<<<< Question 5 >>>>>>>>>>>>>>>>>>>
+
+fruits = ["Apple","Banana","Mango","Orange","Grapes"]
+#  complete list
+print("Fruit list:", fruits)
+
+# first element
+print("First fruit:", fruits[0])
+
+# last element
+print("Last fruit:", fruits[-1])
+
+# total number of items in the list
+print("Total fruits:", len(fruits))
+  
+# <<<<<<<<<<<<<<<< Question 6 >>>>>>>>>>>>>>>>>
+
+numbers = [10, 20, 30, 40, 50]
+
+# Adding 60 to the list
+numbers.append(60)
+
+# Remove 20 from the list
+numbers.remove(20)
+print("Updated list:", numbers)
+
+#  <<<<<<<<<<<<<<< Question 7 >>>>>>>>>>>>>>>>>>>>>>
+
+''' artificial intelligence cann be said when a computer process eveyrthing on its own , calculation 
+to output everythign is done only by computer itself , in modern day it is something that is very important in life 
+ four real life  application of ai is 
+ 1. used in medical sciende
+ 2. used in chatbots 
+ 3. used to automate complex things
+ 4. used in astrophysics'''
+
+#  <<<<<<<<<<<<<<<<<<< Question 8 >>>>>>>>>>>>> 
+
+# ChatGPT 
+# It understands questions given to it and then give human like answers like me hehe, so yeah ai 
+
+# Google Maps route prediction 
+# It analyzes traffic and analyzes toll and distance idk , but it is ai
+
+# Calculator 
+#  obviously not ai 
+
+# Netflix recommendations 
+# It recommends movies and shows based on u
+
+# Voice assistants (Alexa/Siri) 
+# They understand voice commands and give response
